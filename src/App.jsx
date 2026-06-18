604c74c430a6d45c0c5b431c4c8db4f646f1f591
Элина Коновалова <konovalova.elina@gmail.com>
2026-06-18 03:32:48 +0300

    Homework 1,2_

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Layout from './layout/Layout';
 import Home from './sections/home/Home';
 import Blog from './sections/blog/Blog';
 import ModalPost from './components/modal/ModalPost';
+import Reviews from './sections/reviews/Reviews';
+import Works from './sections/works/Works';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/works" element={<Works />}/>
+      <Route path="/reviews" element={<Reviews />}/>
       <Route path="/blog" element={<Blog />}/>
-      <Route path="/blog/post/:id" element={<ModalPost />} />
+      <Route path="/blog/post/:id" element={<ModalPost />}/>
     </Routes>
   );
 }
---
+++ src/sections/reviews/Reviews.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../layout/Layout';
+
+export default function Reviews() {
+  return (
+    <Layout>
+      <div className='reviews' style={{margin: '15% 35%'}}>
+        <h2>Раздел находится в разработке</h2>
+      </div>
+    </Layout>
+  )
+}
